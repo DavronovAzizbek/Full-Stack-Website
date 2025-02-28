e85815d9df038b2d5c8dd7e45b9e66d920d90a93
githubusername <azizbekdavronov1110@gmail.com>
2025-02-28 20:58:31 +0500

    Dashboard section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
 import "./App.css";
+
 
 function App() {
   
+  const router = createBrowserRouter([
+    {
+      path: "/"
+      element: (
+        <>
+        <Home/>
+        </>
+      )
+   }
+ ])
 
   return (
-    <></>
+    <>
+    <RouterProvider router={router} />
+    </>
   );
 }
 
